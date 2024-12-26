e92a3fcd32b560a914a1d8bf015449971132b2fa
martin <guerreiromartin@gmail.com>
2024-12-26 12:05:02 -0300

    first commit

---
+++ RecetasApi/Data/MongoDbService.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace RecetasApi.Data
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase? _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            var connectingString = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? configuration.GetConnectionString("DbConnection");
+            var mongoUrl = MongoUrl.Create(connectingString);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName ?? "RecipesDB");
+        }
+
+        public IMongoDatabase? Database => _database;
+    }
+}
---
+++ RecetasApi/Entities/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RecetasApi.Entities;
+
+public class Recipe
+{
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("title"), BsonRepresentation(BsonType.String)]
+    public string Title { get; set; }
+
+    [BsonElement("description")]
+    public List<string> Description { get; set; }
+}
+
